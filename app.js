7b43a3f912889b49bf18ba74e08b29142961d0fb
deepak <dk721572@gmail.com>
2020-04-29 23:18:18 +0530

    added strucure

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
+let items = []
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-  res.send('hello')
+  res.render('index', { name: 'depz', newItems: items })
 })
 
-app.listen(3000, () => {
-  console.log('Server started 😎');
+app.post('/', function (req, res) {
+
+  let item = {
+    message: req.body.message,
+    number: req.body.number,
+    date: req.body.date,
+    time: req.body.time
+  }
+
+  items.push(item)
+  res.redirect('/')
 
 })
+app.listen(3000, () => console.log('listening on port 3000'))
