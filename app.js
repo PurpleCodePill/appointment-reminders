3ac8c3b8d6bc9c1281bf5d08889085011bea0eae
deepak <dk721572@gmail.com>
2020-04-29 19:48:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hello')
+})
+
+app.listen(3000, () => {
+  console.log('Server started 😎');
+
+})
